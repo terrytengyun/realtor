aebfc74c6f418740524074e091ff9610ed900075
terry-teng <yten@clubautoltd.com>
2018-06-28 16:43:26 -0400

    upload to AWS

--- src/main/java/mercury/cloud/realtor/rest/controllers/RealtorController.java
+++ src/main/java/mercury/cloud/realtor/rest/controllers/RealtorController.java
@@ -3,6 +3,7 @@ package mercury.cloud.realtor.rest.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import mercury.cloud.realtor.rest.entities.Realtor;
 import mercury.cloud.realtor.rest.services.RealtorService;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class RealtorController {
 	
---
+++ src/test/java/mercury/cloud/realtor/rest/AWSTest.java
@@ -0,0 +1,56 @@
+package mercury.cloud.realtor.rest;
+
+import java.io.File;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+public class AWSTest {
+
+	public static void main(String[] args) {
+		Regions clientRegion = Regions.CA_CENTRAL_1;
+        String bucketName = "charliehe";
+        String fileObjKeyName = "ABC.jpg";
+        String fileName = "C:\\Development\\realtors\\realtorSite\\realtor.front\\dest\\images\\test2.jpg";
+
+        try {
+        	AWSCredentials credentials = new BasicAWSCredentials("AKIAI6ZCMYW3ZVDMRJGQ", "RsspGP/b3Z8NVB6p1rF4qErow+C2KHHp3RpYPZLi");
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withRegion(clientRegion)
+                    .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                    .build();
+        
+            // Upload a text string as a new object.
+            //s3Client.putObject(bucketName, stringObjKeyName, "Uploaded String Object");
+            
+            // Upload a file as a new object with ContentType and title specified.
+            PutObjectRequest request = new PutObjectRequest(bucketName, fileObjKeyName, new File(fileName));
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType("plain/text");
+            metadata.addUserMetadata("x-amz-meta-title", "someTitle");
+            request.setMetadata(metadata);
+            s3Client.putObject(request);
+        }
+        catch(AmazonServiceException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process 
+            // it, so it returned an error response.
+            e.printStackTrace();
+        }
+        catch(SdkClientException e) {
+            // Amazon S3 couldn't be contacted for a response, or the client
+            // couldn't parse the response from Amazon S3.
+            e.printStackTrace();
+        }
+
+	}
+
+}
